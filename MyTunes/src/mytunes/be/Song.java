28f772b118f1841e0304b61370d2146e10f97e35
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-11-27 12:29:45 +0100

    Song be

--- MyTunes/src/mytunes/be/Song.java
+++ MyTunes/src/mytunes/be/Song.java
@@ -11,4 +11,58 @@ package mytunes.be;
  */
 public class Song {
     
+    String title;
+    String album;
+    String artist;
+    int year;
+    String genre;
+
+    
+    
+    
+    
+    
+    /**
+     * get the songs title
+     * @return a String with the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *get the songs album
+     * @return a String with the album
+     */
+    public String getAlbum() {
+        return album;
+    }
+
+    /**
+     * get the songs artist
+     * @return a String with the artist
+     */
+    public String getArtist() {
+        return artist;
+    }
+
+    /**
+     * get the songs year
+     * @return a int with the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * get the songs genre
+     * @return a String with the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+    
+    
+    
+    
 }
