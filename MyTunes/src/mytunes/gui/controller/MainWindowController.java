c267dabfb168d5d547db09b28c41b39c8496574d
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-11-27 11:18:37 +0100

    FXML

--- MyTunes/src/mytunes/gui/controller/MainWindowController.java
+++ MyTunes/src/mytunes/gui/controller/MainWindowController.java
@@ -12,26 +12,69 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-import mytunes.gui.controller.MainWindowModel;
-
 /**
  *
  * @author janvanzetten
  */
 public class MainWindowController implements Initializable {
     
-    @FXML
-    private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Label lblSongTitleTopBar;
+    @FXML
+    private Label lblSongAlbumTopBar;
+    @FXML
+    private Label lblSongArtistTopBar;
+    @FXML
+    private Label lblChosenPlaylist;
+    @FXML
+    private Label lblPlaylistInfo;
+    @FXML
+    private Label lblChosenPlaylist1;
+    @FXML
+    private Label lblCurrentTime;
+    @FXML
+    private Label lblTotalTimeSong;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void playSongAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void pauseSongAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void previusSongAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextSongAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void repeatSongsAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void shuffleSongsAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void addSongAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void addPlaylistAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void deletePlaylistAction(ActionEvent event) {
+    }
     
 }
