cbab34411f8739320f67ff0eff9ea78e8e600c28
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-12-06 14:45:32 +0100

    removed bug finding system.out.println

--- MyTunes/src/mytunes/gui/controller/MainWindowController.java
+++ MyTunes/src/mytunes/gui/controller/MainWindowController.java
@@ -382,10 +382,8 @@ public class MainWindowController implements Initializable {
 
         if (listViewPlaylists.getSelectionModel().getSelectedItem() != null) {
             model.setChosenPlaylist(listViewPlaylists.getSelectionModel().getSelectedItem());
-            System.out.println(model.getChosenPlaylist().getPlaylistId());
         }
         if (tblviewMaster.getSelectionModel().getSelectedItem() != null) {
-            model.setChosenSong(tblviewMaster.getSelectionModel().getSelectedItem());
         }
 
 //        if (listViewPlaylists.getSelectionModel().getSelectedItem() != null && tblviewMaster.getSelectionModel().getSelectedItem() != null) {
