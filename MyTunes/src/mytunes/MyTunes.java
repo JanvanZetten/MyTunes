f0298fbb56a8c329c640f709b74ebbcda1b126d9
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-12-13 12:38:26 +0100

    preloader not starting

--- MyTunes/src/mytunes/MyTunes.java
+++ MyTunes/src/mytunes/MyTunes.java
@@ -5,50 +5,19 @@
  */
 package mytunes;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import mytunes.gui.controller.MainWindowController;
+import com.sun.javafx.application.LauncherImpl;
 
 /**
  *
- * @author Alex, Asbjørn og Jan
+ * @author janvanzetten
  */
-public class MyTunes extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        //Get window.
-        Stage newStage = new Stage();
-        FXMLLoader fxLoader = new FXMLLoader(getClass().getResource("gui/view/MainWindow.fxml"));
-        Parent root = fxLoader.load();
-
-        //Instanciate Scene.
-        Scene scene = new Scene(root);
-
-        //Set Stage properties.
-        newStage.setScene(scene);
-        newStage.setTitle("MyTunes");
-        newStage.getIcons().add(new Image("mytunes/gui/view/pictures/logo.png"));
-        newStage.setMinWidth(836);
-        newStage.setMinHeight(606);
-
-        //Get controller and run afterInitialize.
-        MainWindowController cont = fxLoader.getController();
-        cont.afterInitialize();
-
-        //Show Stage
-        newStage.show();
-    }
+public class MyTunes {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        LauncherImpl.launchApplication(MyTunesApplicationStart.class, MyTunesPreloader.class, args);
     }
 
 }
